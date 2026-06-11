e44ffda13ae8aa720fb89c844264f4f4c6cf9d97
Andrei Belskii <mrandrewer@yandex.ru>
2026-06-11 22:39:08 +0300

    Fix layout sizing

--- src/main/java/com/hse/lab4/App.java
+++ src/main/java/com/hse/lab4/App.java
@@ -17,8 +17,10 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("primary"), 900, 650);
         stage.setScene(scene);
+        stage.setMinWidth(800);
+        stage.setMinHeight(620);
         stage.show();
     }
 
--- src/main/java/com/hse/lab4/PrimaryController.java
+++ src/main/java/com/hse/lab4/PrimaryController.java
@@ -2,11 +2,33 @@ package com.hse.lab4;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 
-public class PrimaryController {
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.application.Platform;
+
+public class PrimaryController implements Initializable {
+
+    @FXML
+    private StackPane imagePane;
+
+    @FXML
+    private ImageView imgLogo;
 
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Platform.runLater(() -> {
+            if (imgLogo != null && imagePane != null) {
+                imgLogo.fitWidthProperty().bind(imagePane.widthProperty());
+            }
+        });
     }
 }
